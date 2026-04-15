5c382cce4696629bb91082acc66288779028cfa5
khirul-alame <yeasin1983@gmail.comm>
2026-04-15 19:41:21 +0600

    Social link update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import Link from "next/link";
+import { Github, Twitter, Linkedin, Users } from "lucide-react";
+import {
+  FaInstagramSquare,
+  FaGithub,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const Footer = () => {
-    return (
-        <div>
-            this is
+  return (
+    <footer className="bg-[#2b8e6d] border-gray-100 pt-16 pb-8">
+      <div className="">
+        <h2>KeenKeeper</h2>
+        <p>
+          Your personal shelf of meaningful connections. Browse, tend, and
+          nurture the relationships that matter most.
+        </p>
+      </div>
+      <div className="">
+        <p>Social Links</p>
+        <div className="">
+          <FaInstagramSquare
+            size={24}
+            className="text-pink-600 hover:text-pink-800 transition"
+          />
+
+          <FaGithub size={24} />
+          <FaTwitter size={24} />
+          <FaLinkedin size={24} />
         </div>
-    );
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
